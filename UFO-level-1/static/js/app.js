27f0d77133ad6885858083d37c740d9be4596aa8
mpetersen49 <mpetersen49@gmail.com>
2021-04-12 17:10:35 -0500

    Code working to append table data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
+console.log("app.js loaded");
+
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+
+
+function init() {
+    var tbody = d3.select("tbody");
+    tableData.forEach(sighting => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+function runEnter(event) {
+    d3.event.preventDefault();
+
+    var tbody = d3.select("tbody");
+
+    var inputText = d3.select("#datetime");
+
+    var inputDate = inputText.property("value");
+    var defaultDate = inputText.property("placeholder");
+
+    console.log(inputDate);
+    //console.log(defaultDate);
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputDate);
+    //var filteredData = tableData.filter(sighting => sighting.datetime < defaultDate);
+    console.log(filteredData);
+
+    filteredData.forEach(sighting => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+init();
+
--- UFO-level-1/static/js/data.js
+++ UFO-level-1/static/js/data.js
@@ -1,3 +1,5 @@
+console.log("data.js loaded");
+
 var data = [{
     datetime: "1/1/2010",
     city: "benton",
